657461eedfd0dea41cd9cabe42f6b3571353f49d
cloudsecdee@gmail.com <19dishen@gmail.com>
2025-02-15 15:26:26 +0000

    updated terraform

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
     }
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "eks-devsecops-pipeline-project-1"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
